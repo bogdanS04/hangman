f45c69d5031dcc5af015936a61e77aab724f90d2
bogdanS04 <67114381+bogdanS04@users.noreply.github.com>
2022-07-14 18:28:47 +0300

    Delete data.py

--- data.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-URL = "https://opentdb.com/api.php?amount=10&category=19&type=boolean"
-
-request = requests.get(url=URL)
-request.raise_for_status()
-question_data = request.json()['results']
